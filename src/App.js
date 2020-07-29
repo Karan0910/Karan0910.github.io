0353a89dceafd232b35e6b4e33ec075e9961c944
Karan Sharma <95sharma.karan@gmail.com>
2020-07-29 00:28:10 -0400

    Add intial code

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn How to react 
         </a>
       </header>
     </div>
