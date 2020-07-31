f39b5dc61ddf479f766ca2b936316451519aa44b
Karan Sharma <95sharma.karan@gmail.com>
2020-07-31 03:25:50 -0400

    Major modifications

---
+++ precache-manifest.259be3327a888b149d5381c3dc484314.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "592e2069c08dcd6b0c601e6b64447f67",
+    "url": "/index.html"
+  },
+  {
+    "revision": "177c18a6af33a7fab5a5",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "d59a0dd15878508a98cd",
+    "url": "/static/js/2.f9fa8d80.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/static/js/2.f9fa8d80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "177c18a6af33a7fab5a5",
+    "url": "/static/js/main.4f7cacde.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  }
+]);
